c135cd933e7f3672beeb4b7b96a5f71ca127d1b0
Paige Rudnick <paige.rudnick@icloud.com>
2022-01-11 22:15:24 -0600

    fixing routing

--- webpack.config.js
+++ webpack.config.js
@@ -97,7 +97,7 @@ module.exports = function(_env, argv) {
           { from: 'public/favicon.svg', to: 'favicon.svg' },
           { from: 'public/robots.txt', to: 'robots.txt' },
           { from: 'public/manifest.json', to: 'manifest.json' },
-          { from: '404.html', to: '404.html' }
+          { from: 'public/404.html', to: '404.html' }
         ]
       })
     ].filter(Boolean),
